d0fbffb18f6c5a5842cedb4be9e7223ded720dce
killertechnology <killertechnology@gmail.com>
2019-03-28 12:47:05 -0700

    name updates

--- src/ScorpionHQ/Startup.cs
+++ src/ScorpionHQ/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Scorpiony
+namespace ScorpionHQ
 {
     public class Startup
     {
